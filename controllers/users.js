9eb676a3a080e5efe6de2e779a1d3b4ec85a75cc
RomanRiyanov <yezgo123@gmail.com>
2022-09-12 16:40:38 +0500

    [main] автотесты

--- controllers/users.js
+++ controllers/users.js
@@ -7,28 +7,41 @@ const ERROR = 500;
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch((error) => res.status(NOT_FOUND_ERROR).send({ message: 'Пользователей не найдено' }, error));
+    .catch(() => res.status(NOT_FOUND_ERROR).send({ message: 'Пользователей не найдено' }));
 };
 
 const getUserById = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => res.send({ data: user }))
-    .catch((error) => res.status(NOT_FOUND_ERROR).send({ message: 'Пользователь не найден' }, error));
+    .catch(() => res.status(NOT_FOUND_ERROR).send({ message: 'Пользователь не найден' }));
 };
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-  if (name && about && avatar && name.length >= 2 && name.length <= 30 && about.length >= 2 && about.length <= 30) {
+  if (name
+    && about
+    && avatar
+  // && name.length >= 2
+  // && name.length <= 30
+  //  && about.length >= 2
+  //  && about.length <= 30
+  ) {
     User.create({ name, about, avatar })
       .then((user) => res.send(user))
-      .catch((error) => res.status(INPUT_ERROR).send({ message: 'Пользователь не найден' }, error));
+      .catch(() => res.status(INPUT_ERROR).send({ message: 'Пользователь не найден' }));
   } else res.status(INPUT_ERROR).send({ message: 'Неверно введены данные' });
 };
 
 const updateUser = (req, res) => {
   const { name, about } = req.body;
 
-  if (name && about && name.length >= 2 && name.length <= 30 && about.length >= 2 && about.length <= 30) {
+  if (name
+     && about
+  //  && name.length >= 2 &&
+  //  name.length <= 30 &&
+  //   about.length >= 2 &&
+  //    about.length <= 30
+  ) {
     User.findByIdAndUpdate(
       req.user._id,
       { name, about },
@@ -39,7 +52,7 @@ const updateUser = (req, res) => {
       },
     )
       .then((user) => res.send(user))
-      .catch((error) => res.status(INPUT_ERROR).send({ message: 'Пользователь не найден' }, error));
+      .catch(() => res.status(INPUT_ERROR).send({ message: 'Пользователь не найден' }));
   } else res.status(INPUT_ERROR).send({ message: 'Неверно введены данные' });
 };
 
@@ -57,7 +70,7 @@ const updateAvatar = (req, res) => {
       },
     )
       .then((user) => res.send(user))
-      .catch((error) => res.status(INPUT_ERROR).send({ message: 'Пользователь не найден' }, error));
+      .catch(() => res.status(INPUT_ERROR).send({ message: 'Пользователь не найден' }));
   } else res.status(ERROR).send({ message: 'Неверно введены данные' });
 };
 
